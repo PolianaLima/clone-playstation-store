a087402e38543608eb48e006b78e4a5f29e7f0d8
Poliana <pollyannati@hotmail.com>
2023-05-16 14:57:35 -0300

    Definicao do layout dos componentes

---
+++ src/app/components/card/card-princing/card-princing.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-princing',
+  templateUrl: './card-princing.component.html',
+  styleUrls: ['./card-princing.component.css']
+})
+export class CardPrincingComponent implements OnInit {
+
+  @Input()
+  gameType: string = "Digital PS4";
+  @Input()
+  gamePrice: string = "R$ 399,90";
+
+  constructor(){}
+  ngOnInit(): void {
+  }
+
+}
